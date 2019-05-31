cca184b45f3b2f3134da68baa99330052561b535
dgogovsky <dgogovsky@gmail.com>
2019-05-31 13:33:04 +0300

    подзадача 1 сделана

--- block1.php
+++ block1.php
@@ -6,3 +6,13 @@ print 'I\'m fine.';
 ящей из двух гамбургеров по 4,95 доллара каждый, одного молочно-шоколадного коктейля
 за 1,95 доллара и одной порции кока-колы за 0,85 доллара. Ставка налога на добавленную
 стоимость составляет 7,5%, а чаевые без вычета налогов — 16%.*/
+$hamburger = 4.95;
+$shake = 1.95;
+$cola = 0.85;
+$tip_rate = 0.16;
+$tax_rate = 0.075;
+$food = (2 * $hamburger) + $shake + $cola;
+$tip = $food * $tip_rate;
+$tax = $food * $tax_rate;
+$total = $food + $tip + $tax;
+print 'The total cost of the meal is $' . $total;
